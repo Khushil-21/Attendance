697cba0267491fa93711bc3107b0804b5ff48569
Khushil-21 <khushilshah2105@gmail.com>
2023-09-04 18:39:11 +0530

    Added components to react app

--- src/App.js
+++ src/App.js
@@ -7,8 +7,11 @@ import { Footer } from "./Components/Footer";
 function App() {
 	return (
 		<div className="App">
+			{/* Navbar Component we will reuse it in all webpages */}
 			<Navbar/>
+			{/* Background Component we will reuse it in all webpages */}
 			<Background />
+			{/* Footer Component we will reuse it in all webpages */}
 			<Footer/>
 		</div>
 	);
