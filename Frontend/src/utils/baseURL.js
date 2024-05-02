2933115fbd35c14a5dc6e574c39bc3fd6f94a50b
Khushil-21 <khushilshah2105@gmail.com>
2024-05-02 16:03:33 +0530

    temp connection

--- Frontend/src/utils/baseURL.js
+++ Frontend/src/utils/baseURL.js
@@ -1,12 +1,13 @@
 export default function getBaseUrl(url) {
 	console.log(process.env.NODE_ENV);
-	console.log(process.env.BASE_URL);
+	console.log(process.env.BACKEND_URL);
+	console.log(`${process.env.BACKEND_URL}/${url}`);
 	// Assuming you have different base URLs for development, staging, and production environments
 	if (process.env.NODE_ENV === "development") {
 		return `http://localhost:5001/${url}`; // Update this with your local server URL
 	} else if (process.env.NODE_ENV === "staging") {
 		return `https://staging.example.com/${url}`; // Update this with your staging server URL
 	} else {
-		return process.env.BASE_URL + `${url}`; // Update this with your production server URL
+		return `https://production-attendance-backend.vercel.app/${url}`; // Update this with your production server URL
 	}
 }
